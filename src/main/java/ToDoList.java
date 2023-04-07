94a050a523b055bb6ae8758d0df02509aa1ab2ba
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2023-04-07 12:53:30 +0700

    init commit

---
+++ src/main/java/ToDoList.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author MinhDunk
+ */
+public class ToDoList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
